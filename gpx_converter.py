f33e745d8856d77629407a9fbdd6104cb70da6d4
Matthias Sommer <matthias.sommer@posteo.de>
2019-01-19 11:36:13 +0100

    - add xml header - add validation - round coordinates to six decimals

--- gpx_converter.py
+++ gpx_converter.py
@@ -19,13 +19,17 @@ import csv
 import re
 
 # read csv file
-file = open('C:/Users/Matthias/Desktop/GC-Barny-Extractor/barny_26-09-17.csv', 'r')
-reader = csv.reader(file, delimiter=',')
+file = open('.\\barny_01_19.csv', mode='r', encoding='utf-8-sig')
+reader = csv.reader(file, delimiter=';')
 
-gpxFile = '<gpx>'
+gpxFile = '<?xml version="1.0" encoding="utf-8"?>\n<gpx xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="1.0" creator="Groundspeak Pocket Query" xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd http://www.groundspeak.com/cache/1/0/1 http://www.groundspeak.com/cache/1/0/1/cache.xsd" xmlns="http://www.topografix.com/GPX/1/0">\n'
 
 for row in reader:  
     gccode = row[0]
+    if ('GC' not in gccode): 
+      print (gccode)
+      continue
+
     lat = row[-2]
     lon = row[-1]
     
@@ -44,14 +48,14 @@ for row in reader:
     # convert decimal minutes to decimal degrees: degree + minutes/60, e.g. 33+(41.876/60)
     convertedLat = int(latDeg) + float(latMin)/60
     if ('S' in lat):
-        convertedLat = convertedLat *(-1)
+        convertedLat = convertedLat * (-1)
     
     convertedLon = int(lonDeg) + float(lonMin)/60
     if ('W' in lon):
         convertedLon = convertedLon * (-1)
     
     # build row and append to gpx, e.g. <wpt lat="33.697933" lon="-117.954950"><name>GC10020</name></wpt>
-    convertedRow = '<wpt lat="' + str(convertedLat) +'" lon="' + str(convertedLon) + '"><name>' + gccode + '</name></wpt>'
+    convertedRow = '<wpt lat="' + "{0:.6f}".format(convertedLat) +'" lon="' + "{0:.6f}".format(convertedLon) + '"><name>' + gccode + '</name></wpt>\n'
     gpxFile += convertedRow
 
 file.close()
@@ -59,6 +63,6 @@ file.close()
 gpxFile += '</gpx>'
 
 #  write to file
-text_file = open("barny_26-09-17.gpx", "w")
+text_file = open(".\\barny_01_19.gpx", "w")
 text_file.write(gpxFile)
 text_file.close()
